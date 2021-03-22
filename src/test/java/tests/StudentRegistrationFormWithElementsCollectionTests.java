feae84cbc05ecbe93d80780f9719000173b10616
svasenkov <aanher@gmail.com>
2021-03-22 22:18:54 +0300

    elementscollections

---
+++ src/test/java/tests/StudentRegistrationFormWithElementsCollectionTests.java
@@ -0,0 +1,103 @@
+package tests;
+
+import com.codeborne.selenide.SelenideElement;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static utils.FileUtils.readStringFromFile;
+
+public class StudentRegistrationFormWithElementsCollectionTests {
+
+    static String firstName = "Egor",
+            lastName = "Alexov",
+            email = "aa@aa.com",
+            gender = "Other",
+            mobile = "1234567890",
+            monthOfBirth = "May",
+            yearOfBirth = "1988",
+            dayOfBirth = "10",
+            dayOfWeekOfBirth = "Tuesday",
+            subject1 = "Chemistry",
+            subject2 = "Commerce",
+            hobby1 = "Sports",
+            hobby2 = "Reading",
+            hobby3 = "Music",
+            picture = "1.png",
+            currentAddress = "Montenegro 123",
+            state = "Uttar Pradesh",
+            city = "Merrut";
+
+    static Map<String, String> expectedData = new HashMap<String, String>() {{
+        put("Student Name", firstName + " " + lastName);
+        put("Student Email", email);
+        put("Gender", gender);
+        put("Mobile", mobile);
+        put("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth);
+        put("Subjects", subject1 + ", " + subject2);
+    }};
+
+    @Test
+    void successfulRegistrationTest() {
+
+        // Arrange / Given / Open site
+        open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        // Act / When / Fill the form
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(email);
+        $("#genterWrapper").$(byText(gender)).click();
+
+        $("#userNumber").val(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(String.format("[aria-label='Choose %s, %s %sth, %s']",
+                dayOfWeekOfBirth, monthOfBirth, dayOfBirth, yearOfBirth)).click();
+        $("#subjectsInput").val(subject1).pressEnter();
+        $("#subjectsInput").val(subject2);
+        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby3)).click();
+        $("#uploadPicture").uploadFromClasspath("img/" + picture);
+        $("#currentAddress").val(currentAddress);
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        SoftAssertions softly = new SoftAssertions();
+        for(SelenideElement element: $$(".table-responsive tbody tr").snapshot()) {
+            String key = element.$("td").getText();
+            String actualValue = element.$("td", 1).getText();
+            String expectedValue = expectedData.get(key);
+
+            softly.assertThat(actualValue).isEqualTo(expectedValue);
+        }
+        softly.assertAll();
+    }
+
+}
